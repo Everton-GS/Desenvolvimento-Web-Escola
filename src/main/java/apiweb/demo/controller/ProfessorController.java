417b273de0bcedede0a5badb2d0bc660fe317a84
Everton gs <everton.gs.br1@gmail.com>
2023-08-07 10:24:45 -0300

    Implementação da classe professorController

--- src/main/java/apiweb/demo/controller/ProfessorController.java
+++ src/main/java/apiweb/demo/controller/ProfessorController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,7 +31,7 @@ public class ProfessorController {
                   return ResponseEntity.ok(professor);
     }
 
-    
+    @PutMapping("/atualizar")
     public ResponseEntity<Professor>alterar(Professor professor, @PathVariable Long id,@PathVariable String telefone){
          Professorservice.alterarContato(professor, id, telefone);
              return ResponseEntity.ok().build();
